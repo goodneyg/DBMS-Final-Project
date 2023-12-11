85a86f2e4cc775dc95376e41511d2428a7666b14
goodneyg <33007390+goodneyg@users.noreply.github.com>
2023-12-11 14:20:20 -0500

    cleanup & comments

--- task-manager.js
+++ task-manager.js
@@ -1,6 +1,6 @@
 //Garrett Goodney & Ben Utter
 //Database Systems G01
-//12/7/23
+//12/11/23
 //DBMS Final Project
 
 const express = require('express');
@@ -32,30 +32,30 @@ db.connect((err) => {
 });
 
 //register
-app.post('/register', (req, res) => {
+app.post('/register', (req, res) => {       // "/register" is the endpoint
     const userData = req.body;
-    const insertUserQuery = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    const values = [userData.username, userData.password];
+    const insertUserQuery = 'INSERT INTO users (username, password) VALUES (?, ?)'; //this is the actual sql query
+    const values = [userData.username, userData.password];  //variables that replace the question marks in the query
 
     db.query(insertUserQuery, values, (err, result) => {
-        console.log('user registered');
-        res.json({ message: 'user registered' });
+        console.log('user registered');          //inspect element console log
+        res.json({message: 'user registered'});    //response to the JS server
     });
 });
 
 
 //login
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
     const getUserQuery = 'SELECT * FROM users WHERE username = ? AND password = ?';
     const values = [username, password];
 
     db.query(getUserQuery, values, (err, results) => {
-        if (results.length > 0) {
-            const user = results[0];
-            res.json({ message: 'Login successful', user });
+        if (results.length > 0) {                                   // this is for error checking, it will choose the first user that shows up in
+            const user = results[0];                                // the sql query. for exammple if I am "test", then I shouldnt be matched up with
+            res.json({ message: 'Login successful', user });        // the user "test1" just because it has a matching first half.
         } else {
-            res.status(401).json({ error: 'Invalid credentials' });
+            res.status(401).json({error: 'Invalid credentials'});
         }
     });
 });
@@ -64,13 +64,12 @@ app.post('/login', (req, res) => {
 app.post('/addTask', (req, res) => {
     const taskData = req.body;
     const userId = req.headers['user-id'];
-
     const insertTaskQuery = 'INSERT INTO tasks (taskText, taskTime, taskDay, userId) VALUES (?, ?, ?, ?)';
     const values = [taskData.taskText, taskData.taskTime, taskData.taskDay, userId];
 
     db.query(insertTaskQuery, values, (err, result) => {
         console.log('task saved');
-        res.json({ message: 'task saved' });
+        res.json({message: 'task saved'});
     });
 });
 
@@ -89,25 +88,27 @@ app.get('/getTasks', (req, res) => {
 app.delete('/deleteTask/:taskId', (req, res) => {
     const taskId = req.params.taskId;
     const deleteTaskQuery = 'DELETE FROM tasks WHERE id = ?';
+
     db.query(deleteTaskQuery, [taskId], (result) => {
         console.log('task deleted');
-        res.json({ message: 'task deleted' });
+        res.json({message: 'task deleted'});
     });
 });
 
 //edit task
 app.put('/updateTask/:taskId', (req, res) => {
     const taskId = req.params.taskId;
-    const { taskText, taskTime, taskDay } = req.body;
+    const {taskText, taskTime, taskDay} = req.body;
     const updateTaskQuery = 'UPDATE tasks SET taskText = ?, taskTime = ?, taskDay = ? WHERE id = ?';
     const values = [taskText, taskTime, taskDay, taskId];
 
     db.query(updateTaskQuery, values, (err, result) => {
         console.log('task updated');
-        res.json({ message: 'task updated' });
+        res.json({message: 'task updated'});
     });
 });
 
+//console output to show JS is running
 app.listen(port, () => {
-    console.log(`nodeJS running on port: ${port}`);
+    console.log(`nodeJS running on port: ${port}`); 
 });
